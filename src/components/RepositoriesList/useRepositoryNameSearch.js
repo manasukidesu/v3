529ed66d159349a42f7cc8e09898c9a849bc7518
manasukidesu <2360671460@qq.com>
2023-06-10 14:08:24 +0800

    格式

--- src/components/RepositoriesList/useRepositoryNameSearch.js
+++ src/components/RepositoriesList/useRepositoryNameSearch.js
@@ -1,12 +1,12 @@
 import { ref, computed } from 'vue';
 export default function useRepositoryNameSearch(list) {
-  // 过滤
-  let searchKey = ref('');
-  let searchList = computed(() => {
-    return list.value.filter((value) => parseInt(value) === searchKey.value);
-  });
-  return {
-    searchKey,
-    searchList,
-  };
+    // 过滤
+    let searchKey = ref('');
+    let searchList = computed(() => {
+        return list.value.filter((value) => parseInt(value) === searchKey.value);
+    });
+    return {
+        searchKey,
+        searchList,
+    };
 }
--- src/components/RepositoriesList/useUserRepositories.js
+++ src/components/RepositoriesList/useUserRepositories.js
@@ -1,24 +1,24 @@
 import { ref, onMounted, watch } from 'vue';
 
 export default function usrUserRepositories(user) {
-  let list = ref([]);
-  // 模拟请求
-  let getlist = () => {
-    console.log(`output->user`, user.value);
-    let ren = Math.random() * 10;
-    setTimeout(() => {
-      let arr = [1, 2, 3];
-      list.value = arr.map((value) => {
-        return value * ren;
-      });
-    }, 1e3);
-  };
-  // 挂载后调用
-  onMounted(getlist);
-  // 监听用户改变
-  watch(user, getlist);
-  return {
-    list,
-    getlist,
-  };
+    let list = ref([]);
+    // 模拟请求
+    let getlist = () => {
+        console.log(`output->user`, user.value);
+        let ren = Math.random() * 10;
+        setTimeout(() => {
+            let arr = [1, 2, 3];
+            list.value = arr.map((value) => {
+                return value * ren;
+            });
+        }, 1e3);
+    };
+    // 挂载后调用
+    onMounted(getlist);
+    // 监听用户改变
+    watch(user, getlist);
+    return {
+        list,
+        getlist,
+    };
 }
