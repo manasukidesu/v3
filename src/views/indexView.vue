6507daf6ad2d7990037885bb64ce9f38868f48f6
manasukidesu <2360671460@qq.com>
2023-06-23 14:16:13 +0800

    提交

--- src/views/indexView.vue
+++ src/views/indexView.vue
@@ -1,10 +1,15 @@
 <template>
     <h1>HOME</h1>
-    <el-button :icon="Search">点击</el-button>
+    <el-button :icon="Search" @click="handelClick">点击</el-button>
     <SvgIcon name="logo"></SvgIcon>
 </template>
 
 <script setup lang="ts">
+import router from '@/router';
 import { Search } from '@element-plus/icons-vue';
+const handelClick = () => {
+    let routeData = router.resolve({ name: 'chapters', params: { chapters: 'About' } })
+    console.log(`output->routeData`, routeData)
+}
 </script>
 <style></style>
