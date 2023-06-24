089de479bc5b939c84789dedf29da398d6f64007
manasukidesu <2360671460@qq.com>
2023-06-24 17:46:35 +0800

    1.添加空白loading

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+// svg 插件
 import "virtual:svg-icons-register";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+router.isReady().then(() => {
+    app.mount("#app");
+});
